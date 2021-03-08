a032a64eddfb450c643f2ea2ff2eaae9873b6a01
Deniz Akpınar <ddenizakpinar@gmail.com>
2021-03-08 20:29:52 +0300

    Add cors configuration

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,17 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const cors = require("cors");
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://reddroom.netlify.app",
+  optionsSuccessStatus: 200, 
+};
 
 const userRouter = require("./routes/user");
 const categoryRouter = require("./routes/category");
 const noteRouter = require("./routes/note");
 
-app.use("/user", userRouter);
-app.use("/category", categoryRouter);
-app.use("/note", noteRouter);
+app.use("/user", cors(corsOptions), userRouter);
+app.use("/category", cors(corsOptions), categoryRouter);
+app.use("/note", cors(corsOptions), noteRouter);
 
 exports.api = functions.https.onRequest(app);
